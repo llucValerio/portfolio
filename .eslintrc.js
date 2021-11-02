4af19718fc01834e5159fa341e946eda5011faa5
Lluc Valério <lluc.valerio@gmail.com>
2021-11-02 16:59:36 +0100

    Set up project dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'linebreak-style': 0,
+    'eslint linebreak-style': [0, 'error', 'windows']
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portfolio
-My Personal Portfolio
+# My Personal Portfolio
+This is my personal portfolio, with info & link to my projects and my CV.
